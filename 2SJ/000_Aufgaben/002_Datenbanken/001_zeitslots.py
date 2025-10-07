8e5ddb2e40f5ce6611564b6056501177b1a3c26d
fuchs-git <pasvoss@gmail.com>
2025-10-07 14:29:46 +0200

    Aufgabe "Zeitslots" angelegt

---
+++ 2SJ/000_Aufgaben/002_Datenbanken/001_zeitslots.py
@@ -0,0 +1,10 @@
+'''
+Aufgabe:
+Ergänzen Sie die Datenbank prg_fitness um eine weitere Tabelle zeitslot. Hier sollen für jeden Tag der Woche die
+Doppelstunden von 08:00 bis 22:00 bereits initial erfasst sein (nicht überlappend).
+
+Die Idee ist, dass sich später Sporttreibende in diese eintragen können und auch die Möglichkeiten haben zu sehen,
+wie stark das Studio in diesem Zeitslot ausgelastet sein wird.
+'''
+
+
