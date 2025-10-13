20c50275299da52c00c357bad440eb3b294565f9
fuchs-git <pasvoss@gmail.com>
2025-10-13 10:44:08 +0200

    FilmDB - Formatierung

--- 2SJ/000_Aufgaben/002_Datenbanken/filmDB.py
+++ 2SJ/000_Aufgaben/002_Datenbanken/filmDB.py
@@ -24,8 +24,8 @@ try:
         # -------------------------------------------------------------------------------------------------------------
 
         with db_conn.cursor() as cursor:
-                cursor.execute('DROP DATABASE IF EXISTS prg_filmdatenbank;')
-                cursor.execute('CREATE DATABASE prg_filmdatenbank;')
+            cursor.execute('DROP DATABASE IF EXISTS prg_filmdatenbank;')
+            cursor.execute('CREATE DATABASE prg_filmdatenbank;')
 
 except psycopg.DatabaseError as e:
     print(e, type(e))
@@ -46,40 +46,45 @@ try:
         # -------------------------------------------------------------------------------------------------------------
 
         with db_conn.cursor() as cursor:
-                cursor.execute('''
-                    CREATE TABLE person
-                    (
-                        person_id  BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-                        nachname   TEXT NOT NULL,
-                        vorname    TEXT NOT NULL,
-                        geburtstag DATE NOT NULL
-                    );
-                ''')
-                cursor.execute('''
-                   CREATE TABLE film
-                    (
-                        film_id          BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-                        titel            TEXT    NOT NULL,
-                        erscheinungsjahr INTEGER NOT NULL,
-                        fk_regie         BIGINT  NOT NULL REFERENCES person (person_id)
-                            ON UPDATE CASCADE ON DELETE RESTRICT
-                    ); 
-                ''')
-                cursor.execute('''
-                CREATE TABLE hat_mitgespielt_in
-                    (
-                        fk_filme_id  BIGINT REFERENCES film (film_id),
-                        fk_person_id BIGINT REFERENCES person (person_id),
-                        rolle        TEXT NOT NULL,
-                        PRIMARY KEY (fk_filme_id, fk_person_id)
-                    );
-            ''')
-        # -------------------------------------------------------------------------------------------------------------
-        # Tabellen befüllen
-        # -------------------------------------------------------------------------------------------------------------
-                cursor.executemany("INSERT INTO person (nachname, vorname, geburtstag) VALUES (%s,%s,%s)", filmDB_Inhalt.personen_liste)
-                cursor.executemany("INSERT INTO film(titel, erscheinungsjahr, fk_regie) VALUES  (%s, %s, (SELECT person_id FROM person WHERE nachname = %s))", filmDB_Inhalt.filme_liste)
-                cursor.executemany("INSERT INTO hat_mitgespielt_in(fk_filme_id, fk_person_id, rolle) VALUES ((SELECT film_id FROM film WHERE titel LIKE %s), (SELECT person_id FROM person WHERE nachname = %s),%s)", filmDB_Inhalt.rollen_liste)
+            cursor.execute('''
+                           CREATE TABLE person (
+                               person_id  BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+                               nachname   TEXT NOT NULL,
+                               vorname    TEXT NOT NULL,
+                               geburtstag DATE NOT NULL
+                           );
+                           ''')
+            cursor.execute('''
+                           CREATE TABLE film (
+                               film_id          BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+                               titel            TEXT    NOT NULL,
+                               erscheinungsjahr INTEGER NOT NULL,
+                               fk_regie         BIGINT  NOT NULL REFERENCES person (person_id)
+                                   ON UPDATE CASCADE ON DELETE RESTRICT
+                           );
+                           ''')
+            cursor.execute('''
+                           CREATE TABLE hat_mitgespielt_in (
+                               fk_filme_id  BIGINT REFERENCES film (film_id),
+                               fk_person_id BIGINT REFERENCES person (person_id),
+                               rolle        TEXT NOT NULL,
+                               PRIMARY KEY (fk_filme_id, fk_person_id)
+                           );
+                           ''')
+            # ----------------------------------------------------------------------------------------------------------
+            # Tabellen befüllen
+            # ----------------------------------------------------------------------------------------------------------
+            cursor.executemany("INSERT INTO person (nachname, vorname, geburtstag) VALUES (%s,%s,%s)",
+                               filmDB_Inhalt.personen_liste)
+
+            cursor.executemany("""INSERT INTO film(titel, erscheinungsjahr, fk_regie)
+                                  VALUES (%s, %s, (SELECT person_id FROM person WHERE nachname = %s))""",
+                               filmDB_Inhalt.filme_liste)
+
+            cursor.executemany("""INSERT INTO hat_mitgespielt_in(fk_filme_id, fk_person_id, rolle)
+                                  VALUES ((SELECT film_id FROM film WHERE titel LIKE %s),
+                                          (SELECT person_id FROM person WHERE nachname = %s), %s)""",
+                               filmDB_Inhalt.rollen_liste)
 
 except psycopg.DatabaseError as e:
     print(e, type(e))
